1c796f9b98cca342ae6e1f9de467ceac06e8a9ef
Bentar Segara Buana <143939217+BentarSegara@users.noreply.github.com>
2025-06-27 19:46:12 +0700

    Update MainWindow.java

--- MainWindow.java
+++ MainWindow.java
@@ -1,17 +1,15 @@
-package components;
-
-import guitools.Window;
+import guicomponents.Window;
 
 public class MainWindow extends Window{
     protected static Integer confirm;
 
-    protected static Notif p1Notif = new Notif("Player 1 Win, Do you want to restart the game ?", "src/assets/icon/X.png");
-    protected static Notif p2Notif = new Notif("Player 2 Win, Do you want to restart the game ?", "src/assets/icon/O.png");
-    protected static Notif drawNotif = new Notif("Draw, Do you want to restart the game ?", "src/assets/icon/XO.png");
+    protected static Notif p1Notif = new Notif("Player 1 Win, Do you want to restart the game ?", "assets/icon/X.png");
+    protected static Notif p2Notif = new Notif("Player 2 Win, Do you want to restart the game ?", "assets/icon/O.png");
+    protected static Notif drawNotif = new Notif("Draw, Do you want to restart the game ?", "assets/icon/XO.png");
 
     public MainWindow(){
         super("TicTacToe", 380, 270, false);
-        setIconApp("src/assets/icon/XO.png");
+        setIconApp("assets/icon/XO.png");
         setLocationRelativeTo(null);
 
         Board board = new Board(null);        
